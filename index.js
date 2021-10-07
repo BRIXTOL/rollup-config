4741470ca8c9c3671f17704c85d04f2e7e9e2025
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-10-07 18:55:26 +0200

    added multi, replaced typescript2 and export as getters

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import beep from '@rollup/plugin-beep';
 import commonjs from '@rollup/plugin-commonjs';
 import html from '@rollup/plugin-html';
 import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import ts from 'rollup-plugin-typescript2';
-import tspaths from 'rollup-plugin-ts-paths';
+import multi from '@rollup/plugin-multi-entry';
 import polyfills from 'rollup-plugin-node-polyfills';
 import postcss from 'rollup-plugin-postcss';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import ts from '@rollup/plugin-typescript';
+import tspaths from 'rollup-plugin-ts-paths';
 import scss from 'rollup-plugin-scss';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
@@ -16,121 +17,26 @@ import copy from 'rollup-plugin-copy';
 import del from 'rollup-plugin-delete';
 import filesize from 'rollup-plugin-filesize';
 import { terser } from 'rollup-plugin-terser';
-/* -------------------------------------------- */
-/* CORE                                         */
-/* -------------------------------------------- */
 export { defineConfig as rollup } from 'rollup';
 export { config, env, banner, jsonmin } from '@brixtol/rollup-utils';
-/* -------------------------------------------- */
-/* PLUGINS                                      */
-/* -------------------------------------------- */
 export const plugin = {
-    /**
-     * Alias modules in a build.
-     *
-     * [@rollup/plugin-alias](https://git.io/JuTc9)
-     */
-    alias,
-    /**
-     * Beeps when a build ends with errors.
-     *
-     * [@rollup/plugin-beep](https://git.io/JuTEW)
-     */
-    beep,
-    /**
-     * Copy files and folders, with glob support.
-     *
-     * [rollup-plugin-copy](https://git.io/JuTux)
-     */
-    copy,
-    /**
-     * Convert CommonJS modules to ES Modules
-     *
-     * [@rollup/plugin-commonjs](https://git.io/JuTcI)
-     */
-    commonjs,
-    /**
-     * Delete files and folders.
-     *
-     * [rollup-plugin-delete](https://git.io/JuTz3)
-     */
-    del,
-    /**
-     * Show filesize in the cli
-     *
-     * [rollup-plugin-filesize](https://git.io/JuTzw)
-     */
-    filesize,
-    /**
-     * Creates HTML files to serve Rollup bundles
-     *
-     * [@rollup/plugin-html](https://git.io/JuTWL)
-     */
-    html,
-    /**
-     * Convert JSON files to ES Modules.
-     *
-     * [@rollup/plugin-json](https://git.io/JuTni)
-     */
-    json,
-    /**
-     * Live Reload after changes
-     *
-     * [rollup-plugin-livereload](https://git.io/JuTu8)
-     */
-    livereload,
-    /**
-     * Allows the node builtins to be required/imported.
-     *
-     * [rollup-plugin-node-polyfills](https://git.io/JuTuV)
-     */
-    polyfills,
-    /**
-     * Seamless integration between Rollup and PostCSS.
-     *
-     * [rollup-plugin-postcss](https://git.io/JuEZg)
-     */
-    postcss,
-    /**
-     * Use the Node resolution algorithm.
-     *
-     * [@rollup/plugin-node-resolve](https://git.io/JOqCR)
-     */
-    resolve,
-    /**
-     * Replace occurrences of a set of strings.
-     *
-     * [@rollup/plugin-replace](https://git.io/JuTcC)
-     */
-    replace,
-    /**
-     * Seamless integration with Typescript.
-     *
-     * [@rollup/plugin-typescript](https://git.io/JuTng)
-     */
-    ts,
-    /**
-     * Replace alias with resolved import from paths in tsconfig.json
-     *
-     * rollup-plugin-ts-paths](https://git.io/JuTEV)
-     */
-    tspaths,
-    /**
-     * Process SASS and SCSS files
-     *
-     * [rollup-plugin-scss](https://git.io/JuEZp)
-     */
-    scss,
-    /**
-     * Serve the bundle
-     *
-     * [rollup-plugin-serve](https://git.io/JuTuq)
-     */
-    serve,
-    /**
-     * Minify generated es bundle using terser under the hood.
-     * [rollup-plugin-terser](https://git.io/JuTz5)
-     */
-    terser
+    get alias() { return alias; },
+    get beep() { return beep; },
+    get copy() { return copy; },
+    get commonjs() { return commonjs; },
+    get del() { return del; },
+    get filesize() { return filesize; },
+    get html() { return html; },
+    get json() { return json; },
+    get livereload() { return livereload; },
+    get multi() { return multi; },
+    get polyfills() { return polyfills; },
+    get postcss() { return postcss; },
+    get resolve() { return resolve; },
+    get replace() { return replace; },
+    get scss() { return scss; },
+    get serve() { return serve; },
+    get terser() { return terser; },
+    get ts() { return ts; },
+    get tspaths() { return tspaths; }
 };
-;
--- index.ts
+++ index.ts
@@ -3,12 +3,13 @@ import beep from '@rollup/plugin-beep';
 import commonjs from '@rollup/plugin-commonjs';
 import html from '@rollup/plugin-html';
 import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import ts from 'rollup-plugin-typescript2';
-import tspaths from 'rollup-plugin-ts-paths';
+import multi from '@rollup/plugin-multi-entry';
 import polyfills from 'rollup-plugin-node-polyfills';
 import postcss from 'rollup-plugin-postcss';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import ts from '@rollup/plugin-typescript';
+import tspaths from 'rollup-plugin-ts-paths';
 import scss from 'rollup-plugin-scss';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
@@ -17,124 +18,135 @@ import del from 'rollup-plugin-delete';
 import filesize from 'rollup-plugin-filesize';
 import { terser } from 'rollup-plugin-terser';
 
-/* -------------------------------------------- */
-/* CORE                                         */
-/* -------------------------------------------- */
-
 export { defineConfig as rollup } from 'rollup';
 export { config, env, banner, jsonmin } from '@brixtol/rollup-utils';
 
-/* -------------------------------------------- */
-/* PLUGINS                                      */
-/* -------------------------------------------- */
-
+/**
+ * Rollup Plugins
+ *
+ * A
+ */
 export const plugin = {
   /**
    * Alias modules in a build.
    *
    * [@rollup/plugin-alias](https://git.io/JuTc9)
    */
-  alias,
+  get alias () { return alias; },
   /**
    * Beeps when a build ends with errors.
    *
    * [@rollup/plugin-beep](https://git.io/JuTEW)
    */
-  beep,
+  get beep () { return beep; },
   /**
    * Copy files and folders, with glob support.
    *
    * [rollup-plugin-copy](https://git.io/JuTux)
    */
-  copy,
+  get copy () { return copy; },
   /**
    * Convert CommonJS modules to ES Modules
    *
    * [@rollup/plugin-commonjs](https://git.io/JuTcI)
    */
-  commonjs,
+  get commonjs () { return commonjs; },
   /**
    * Delete files and folders.
    *
    * [rollup-plugin-delete](https://git.io/JuTz3)
    */
-  del,
+  get del () { return del; },
   /**
    * Show filesize in the cli
    *
    * [rollup-plugin-filesize](https://git.io/JuTzw)
    */
-  filesize,
+  get filesize () { return filesize; },
   /**
    * Creates HTML files to serve Rollup bundles
    *
    * [@rollup/plugin-html](https://git.io/JuTWL)
    */
-  html,
+  get html () { return html; },
   /**
    * Convert JSON files to ES Modules.
    *
    * [@rollup/plugin-json](https://git.io/JuTni)
    */
-  json,
+  get json () { return json; },
   /**
    * Live Reload after changes
    *
    * [rollup-plugin-livereload](https://git.io/JuTu8)
    */
-  livereload,
+  get livereload () { return livereload; },
+  /**
+   * Allows use of multiple entry points for a bundle.
+   *
+   * [@rollup/plugin-multi-entry](https://git.io/JwRT2)
+   */
+  get multi () { return multi; },
   /**
    * Allows the node builtins to be required/imported.
    *
    * [rollup-plugin-node-polyfills](https://git.io/JuTuV)
    */
-  polyfills,
+  get polyfills () { return polyfills; },
   /**
    * Seamless integration between Rollup and PostCSS.
    *
    * [rollup-plugin-postcss](https://git.io/JuEZg)
    */
-  postcss,
+  get postcss () { return postcss; },
   /**
    * Use the Node resolution algorithm.
    *
    * [@rollup/plugin-node-resolve](https://git.io/JOqCR)
    */
-  resolve,
+  get resolve () { return resolve; },
   /**
    * Replace occurrences of a set of strings.
    *
    * [@rollup/plugin-replace](https://git.io/JuTcC)
    */
-  replace,
-  /**
-   * Seamless integration with Typescript.
-   *
-   * [@rollup/plugin-typescript](https://git.io/JuTng)
-   */
-  ts,
-  /**
-   * Replace alias with resolved import from paths in tsconfig.json
-   *
-   * rollup-plugin-ts-paths](https://git.io/JuTEV)
-   */
-  tspaths,
+  get replace () { return replace; },
   /**
    * Process SASS and SCSS files
    *
    * [rollup-plugin-scss](https://git.io/JuEZp)
+   *
+   * ---
+   *
+   * **DEPRECATED**
+   *
+   * > Use `plugin.postcss` instead. In future versions
+   * this plugin will no longer be available on this export.
+   *
+   * @deprecated
    */
-  scss,
+  get scss () { return scss; },
   /**
    * Serve the bundle
    *
    * [rollup-plugin-serve](https://git.io/JuTuq)
    */
-  serve,
+  get serve () { return serve; },
   /**
    * Minify generated es bundle using terser under the hood.
    * [rollup-plugin-terser](https://git.io/JuTz5)
    */
-  terser
+  get terser () { return terser; },
+  /**
+   * Seamless integration with Typescript.
+   *
+   * [@rollup/plugin-typescript](https://git.io/JuTng)
+   */
+  get ts () { return ts; },
+  /**
+   * Replace alias with resolved import from paths in tsconfig.json
+   *
+   * rollup-plugin-ts-paths](https://git.io/JuTEV)
+   */
+  get tspaths () { return tspaths; }
 };
-;
