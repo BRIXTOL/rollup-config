af6b78486ef62e1a2ad8122495aa5e1c17917bd4
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-10-07 18:54:01 +0200

    add multi entry and replace typescript plugin (again)

--- index.d.ts
+++ index.d.ts
@@ -3,11 +3,13 @@ import beep from '@rollup/plugin-beep';
 import commonjs from '@rollup/plugin-commonjs';
 import html from '@rollup/plugin-html';
 import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import tspaths from 'rollup-plugin-ts-paths';
+import multi from '@rollup/plugin-multi-entry';
 import polyfills from 'rollup-plugin-node-polyfills';
 import postcss from 'rollup-plugin-postcss';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import ts from '@rollup/plugin-typescript';
+import tspaths from 'rollup-plugin-ts-paths';
 import scss from 'rollup-plugin-scss';
 import livereload from 'rollup-plugin-livereload';
 import copy from 'rollup-plugin-copy';
@@ -16,111 +18,24 @@ import { terser } from 'rollup-plugin-terser';
 export { defineConfig as rollup } from 'rollup';
 export { config, env, banner, jsonmin } from '@brixtol/rollup-utils';
 export declare const plugin: {
-    /**
-     * Alias modules in a build.
-     *
-     * [@rollup/plugin-alias](https://git.io/JuTc9)
-     */
-    alias: typeof alias;
-    /**
-     * Beeps when a build ends with errors.
-     *
-     * [@rollup/plugin-beep](https://git.io/JuTEW)
-     */
-    beep: typeof beep;
-    /**
-     * Copy files and folders, with glob support.
-     *
-     * [rollup-plugin-copy](https://git.io/JuTux)
-     */
-    copy: typeof copy;
-    /**
-     * Convert CommonJS modules to ES Modules
-     *
-     * [@rollup/plugin-commonjs](https://git.io/JuTcI)
-     */
-    commonjs: typeof commonjs;
-    /**
-     * Delete files and folders.
-     *
-     * [rollup-plugin-delete](https://git.io/JuTz3)
-     */
-    del: typeof del;
-    /**
-     * Show filesize in the cli
-     *
-     * [rollup-plugin-filesize](https://git.io/JuTzw)
-     */
-    filesize: (options?: import("rollup-plugin-filesize").FileSizePluginOptions) => import("rollup").Plugin;
-    /**
-     * Creates HTML files to serve Rollup bundles
-     *
-     * [@rollup/plugin-html](https://git.io/JuTWL)
-     */
-    html: typeof html;
-    /**
-     * Convert JSON files to ES Modules.
-     *
-     * [@rollup/plugin-json](https://git.io/JuTni)
-     */
-    json: typeof json;
-    /**
-     * Live Reload after changes
-     *
-     * [rollup-plugin-livereload](https://git.io/JuTu8)
-     */
-    livereload: typeof livereload;
-    /**
-     * Allows the node builtins to be required/imported.
-     *
-     * [rollup-plugin-node-polyfills](https://git.io/JuTuV)
-     */
-    polyfills: typeof polyfills;
-    /**
-     * Seamless integration between Rollup and PostCSS.
-     *
-     * [rollup-plugin-postcss](https://git.io/JuEZg)
-     */
-    postcss: typeof postcss;
-    /**
-     * Use the Node resolution algorithm.
-     *
-     * [@rollup/plugin-node-resolve](https://git.io/JOqCR)
-     */
-    resolve: typeof resolve;
-    /**
-     * Replace occurrences of a set of strings.
-     *
-     * [@rollup/plugin-replace](https://git.io/JuTcC)
-     */
-    replace: typeof replace;
-    /**
-     * Seamless integration with Typescript.
-     *
-     * [@rollup/plugin-typescript](https://git.io/JuTng)
-     */
-    ts: import("rollup").PluginImpl<import("rollup-plugin-typescript2/dist/partial").Partial<import("rollup-plugin-typescript2/dist/ioptions").IOptions>>;
-    /**
-     * Replace alias with resolved import from paths in tsconfig.json
-     *
-     * rollup-plugin-ts-paths](https://git.io/JuTEV)
-     */
-    tspaths: typeof tspaths;
-    /**
-     * Process SASS and SCSS files
-     *
-     * [rollup-plugin-scss](https://git.io/JuEZp)
-     */
-    scss: typeof scss;
-    /**
-     * Serve the bundle
-     *
-     * [rollup-plugin-serve](https://git.io/JuTuq)
-     */
-    serve: any;
-    /**
-     * Minify generated es bundle using terser under the hood.
-     * [rollup-plugin-terser](https://git.io/JuTz5)
-     */
-    terser: typeof terser;
+    readonly alias: typeof alias;
+    readonly beep: typeof beep;
+    readonly copy: typeof copy;
+    readonly commonjs: typeof commonjs;
+    readonly del: typeof del;
+    readonly filesize: (options?: import("rollup-plugin-filesize").FileSizePluginOptions) => import("rollup").Plugin;
+    readonly html: typeof html;
+    readonly json: typeof json;
+    readonly livereload: typeof livereload;
+    readonly multi: typeof multi;
+    readonly polyfills: typeof polyfills;
+    readonly postcss: typeof postcss;
+    readonly resolve: typeof resolve;
+    readonly replace: typeof replace;
+    readonly scss: typeof scss;
+    readonly serve: any;
+    readonly terser: typeof terser;
+    readonly ts: typeof ts;
+    readonly tspaths: typeof tspaths;
 };
+//# sourceMappingURL=index.d.ts.map
